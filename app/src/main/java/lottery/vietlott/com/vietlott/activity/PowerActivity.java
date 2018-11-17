b8284691e8809f20b58cfe4087e2c13cfd9a02ab
macbook <macbook@MacBook-Air-cua-admin.local>
2018-11-17 12:07:27 +0700

    update

--- app/src/main/java/lottery/vietlott/com/vietlott/activity/PowerActivity.java
+++ app/src/main/java/lottery/vietlott/com/vietlott/activity/PowerActivity.java
@@ -6,7 +6,10 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import lottery.vietlott.com.vietlott.R;
@@ -21,15 +24,47 @@ public class PowerActivity extends AppCompatActivity {
 
         // Nhập dữ liệu cho Mua bao
         List<String> spinnerArray =  new ArrayList<String>();
-        spinnerArray.add("item1");
-        spinnerArray.add("item2");
+        spinnerArray.add("Chơi thông thường");
+        spinnerArray.add("Bao 5");
+        spinnerArray.add("Bao 7");
+        spinnerArray.add("Bao 8");
+        spinnerArray.add("Bao 9");
+        spinnerArray.add("Bao 10");
+        spinnerArray.add("Bao 11");
+        spinnerArray.add("Bao 12");
+        spinnerArray.add("Bao 13");
+        spinnerArray.add("Bao 14");
+        spinnerArray.add("Bao 15");
+        spinnerArray.add("Bao 18");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, spinnerArray);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner sItems = (Spinner) findViewById(R.id.spinner);
         sItems.setAdapter(adapter);
+
+        // Nhập dữ liệu cho mua theo kì liên tiếp
+        List<String> spinnerDate =  new ArrayList<String>();
+
+        //lấy giá trị của ngày tháng hiện tại cho kì chơi
+        String dateStr = "04/05/2010";
+        SimpleDateFormat curFormater = new SimpleDateFormat("dd/MM/yyyy");
+        Date dateObj = null;
+        try {
+            dateObj = curFormater.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat postFormater = new SimpleDateFormat("dd MMMM, yyyy");
+        String newDateStr = postFormater.format(dateObj);
+
+        // nạp dữ liệu cho spinner của ngày tháng vừa nhận.
+        spinnerArray.add(newDateStr);
+        ArrayAdapter<String> adapterDate = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerDate);
+        adapterDate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItemsDate = (Spinner) findViewById(R.id.spinner2);
+        sItemsDate.setAdapter(adapterDate);
 
     }
 
